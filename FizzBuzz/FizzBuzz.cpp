5ea296037ab791332e9d88e20ce09ffbe51f601f
TamaraVargasG <144067148+TamaraVargasG@users.noreply.github.com>
2025-08-06 10:36:45 -0600

    Add new rules

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-        for (int i = 1; i <= 100; ++i) {
-            if (i % 15 == 0) {
-                cout << "FizzBuzz";
-            }
-            else if (i % 3 == 0) {
-                cout << "Fizz";
-            }
-            else if (i % 5 == 0) {
-                cout << "Buzz";
-            }
-            else {
-                cout << i;
-            }
-            cout << endl;
+    for (int i = 1; i <= 100; ++i) {
+        bool printed = false;
+
+        if (i % 3 == 0) {
+            cout << "Fizz";
+            printed = true;
         }
-        return 0;
+        if (i % 5 == 0) {
+            cout << "Buzz";
+            printed = true;
+        }
+        if (i % 2 == 0) {
+            cout << "Ping";
+            printed = true;
+        }
+        if (i % 7 == 0) {
+            cout << "Pong";
+            printed = true;
+        }
+
+        if (!printed) {
+            cout << i;
+        }
+
+        cout << endl;
     }
 
-
+    return 0;
+}
